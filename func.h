7ed1d80b32fa09b30e486d458be978df773eda55
wuruochong <rwu3@stuy.edu>
2016-10-14 11:46:59 -0400

    Headers done

--- func.h
+++ func.h
@@ -4,3 +4,17 @@ song *table[26];
 song * add_song(char n[], char b[]);
 
 song * search_song(char n[]);
+
+song * search_song(char b[]);
+
+void print_ent(char a);
+
+void print_art(char b[]);
+
+void print_all();
+
+void shuffle(int n);
+
+void del_song(char a[], char b[]);
+
+void del_all();
