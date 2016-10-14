34ed570c1f705c7d618a0207ac9264c6f904e6b1
wuruochong <rwu3@stuy.edu>
2016-10-14 11:38:56 -0400

    Initial files added

---
+++ func.h
@@ -0,0 +1,6 @@
+#include "structs.h"
+song *table[26];
+
+song * add_song(char n[], char b[]);
+
+song * search_song(char n[]);
---
+++ structs.h
@@ -0,0 +1,22 @@
+typedef struct song_node{
+  char name[256];
+  char artist[256];
+  struct song_node *prev;
+  struct song_node *next;
+} song;
+
+song * insert_front(song *a, char n[], char b[]);
+
+song * insert_order(song *a, char n[], char b[]);
+
+void print_list(song *a);
+
+song * find_song(song *a, char n[]);
+
+song * find_song2(song *a, char b[]);
+
+song * rand_song(song *a);
+
+song * remove_song(song *a, char n[]);
+
+song * free_list(song *a);
